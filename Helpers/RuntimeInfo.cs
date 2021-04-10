bd04af6ec311c732f437112a3b336a0aa4897406
Piotr Radlak <p10trek@o2.pl>
2021-04-10 15:59:17 +0200

    WPF app created, added http Client

---
+++ Helpers/RuntimeInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace DesktopNewsApp.Helpers
+{
+    class RuntimeInfo
+    {
+        public string GetMethodName()
+        {
+            var st = new StackTrace();
+            var sf = st.GetFrame(0);
+            return sf?.GetMethod()?.ToString() ?? string.Empty;
+        }
+    }
+}
---
+++ RequestModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesktopNewsApp
+{
+    public class RequestModel
+    {
+        
+        public string Token { get; set; }
+        public string Categories { get; set; }
+        public string Search { get; set;}
+        public int Limit { get; set; }
+        public string Url { get; set; }
+        public int Timeout { get; set; } = -1;
+    }
+}
