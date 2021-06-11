98de6efe8fcad17c375efb6bdb1e4d40de97b850
Piotr Radlak <p10trek@o2.pl>
2021-06-11 08:29:20 +0200

    handling of favorites & History

---
+++ Views/HistListTemplate.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NewsApp.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for HistListTemplate.xaml
+    /// </summary>
+    public partial class HistListTemplate : UserControl
+    {
+        public HistListTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
